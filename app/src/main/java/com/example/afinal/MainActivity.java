51c793db3f0ff0e3b85fea2a8f4bcb8d56643e13
mannev1 <vmanne3@illinois>
2019-12-09 12:14:41 -0600

    final version

--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -20,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-
         EditText vocab1 = findViewById(R.id.vocab1);
         englishArray = new String[5];
 
